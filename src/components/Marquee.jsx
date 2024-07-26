c15e5dc68c719d9ce8b5bfef507b9e3f5b338789
Yashodhan Sonune <152440746+YashodhanSonune@users.noreply.github.com>
2024-07-26 21:54:13 +0530

    changed font and bg color

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 function Marquee() {
   return (
-    <div className='w-full py-10 rounded-tl-3xl rounded-tr-3xl bg-[#1072b8]'>
+    <div className='w-full py-10 rounded-tl-3xl rounded-tr-3xl bg-[#004d43] text-white'>
         <div className='text border-t-2 border-b-2 border-zinc-100 flex gap-10 whitespace-nowrap overflow-hidden'>
             <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease:"linear", repeat:Infinity, duration: 5 }} className='text-[22vw] uppercase leading-none font-["Test_Founders_Grotesk_X-Condensed"] pt-[4vw] mb-[6vw] font-semibold'>We are Ochi</motion.h1>
             <motion.h1 initial={{x:0}} animate={{x: "-100%"}} transition={{ease:"linear", repeat:Infinity, duration: 5 }} className='text-[22vw] uppercase leading-none font-["Test_Founders_Grotesk_X-Condensed"] pt-[4vw] mb-[6vw] font-semibold'>We are Ochi</motion.h1>
